190c629b93e573a47c2373eb6800ca42c3de54ac
Marie Wong <mail@mariewong.net>
2021-10-14 17:01:45 -0700

    update

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,9 +2,9 @@ exports.createPages = ({ actions }) => {
   const { createRedirect } = actions;
 
   createRedirect({
-    fromPath: '/steamup',
+    fromPath: '/steamup/',
     isPermanent: true,
     redirectInBrowser: true,
-    toPath: 'https://www.kickstarter.com/projects/steam-up/steam-up-a-feast-of-dim-sum'
+    toPath: 'https://www.kickstarter.com/projects/steam-up/steam-up-a-feast-of-dim-sum?utm_source=website&utm_medium=steamuppage&utm_campaign=forward'
   });
 };
