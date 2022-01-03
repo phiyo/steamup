daf8adca44c8c2b2c11f489c01743b10c3c1e53c
Marie Wong <mail@mariewong.net>
2022-01-03 13:52:51 -0800

    update

--- src/components/SubscribeTest.js
+++ src/components/SubscribeTest.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SubscribeBoxTest = ({ textAlign }) => (
   <div id="mc_embed_signup">
-    <form action="https://585588ae.sibforms.com/serve/MUIEAEbLBtFILCXJM0NDF8-38xnk2z1U24BVVEju01n5A0RG7ECKLkHRG8Ek4ZqmFeenkalgNODowLbmo3xRxBMdNp9O0D7IRKp_f6Ge7ZZQ_RG3L9nqAf2gL7fk_sn2tMsb-R-urPeKKPFTAqsCK1dT-M3PyPsWkH0KvoTe9GMRIyYchvdcgf4ySnw-1cDPapSvo6AGfnp07zET" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
+    <form action="https://585588ae.sibforms.com/serve/MUIEAOK3ZY-42wD4a2eFV782-GCH4ZTJaF06PajyGrjrWinjiDMue_TUrUzxubESs2w2vZa52iolw9QXtpfN1GI1iHkTwOzHL_aAVsG6u-nVvLRv9AIlWEvLUHq5KceYbsJ3L8spH6yOYfxXG84UqPQjwoEgIGPpM4O1VhE_U3_DF0O2Pjbg-QKkcnf5rh9O9bZjwgmFj8U4dcKb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
       <input type="email" defaultValue="" name="EMAIL" className={`email rounded-sm text-gray-900 w-full p-2 mt-2 mb-4 text-lg xl:max-w-lg ${textAlign}`} id="mce-EMAIL" placeholder="email address" required />
       {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
       <div style={{position: "absolute", left: "-5000px"}}>
